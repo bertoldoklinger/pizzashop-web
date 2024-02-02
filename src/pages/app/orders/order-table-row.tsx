dba4477af611214661857ef75a5d8813f54ffa76
Bertoldo Klinger <bertoldokbrj@gmail.com>
2024-02-02 10:06:13 -0300

    feat: add table, filter and pagination on orders

---
+++ src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,41 @@
+import { ArrowRight, Search, X } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { TableCell, TableRow } from '@/components/ui/table'
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Button variant="outline" size={'xs'}>
+          <Search className="h-3 w-3" />
+          <span className="sr-only">Detalhes do pedido</span>
+        </Button>
+      </TableCell>
+      <TableCell className="font-mono text-sm font-medium">
+        78788rew8r7we87dq
+      </TableCell>
+      <TableCell className="text-muted-foreground">Há 15 minutos</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400" />
+          <span className="font-medium text-muted-foreground">Pendente</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium"> Bertoldo Klinger Barros</TableCell>
+      <TableCell className="font-medium">R$ 149,98</TableCell>
+      <TableCell>
+        <Button variant={'outline'} size={'xs'}>
+          <ArrowRight className="mr-2 h-3 w-3" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant={'ghost'} size={'xs'}>
+          <X className="mr-2 h-3 w-3" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
