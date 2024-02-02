0c6581a342608fb6891ee877fd0c0b6a5a44203d
Bertoldo Klinger <bertoldokbrj@gmail.com>
2024-02-01 22:30:34 -0300

    feat: add header theme-toggle and account-menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,8 @@
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 
+import { AccountMenu } from './account-menu'
 import { NavLink } from './nav-link'
+import { ThemeToggle } from './theme/theme-toggle'
 import { Separator } from './ui/separator'
 
 export function Header() {
@@ -8,6 +10,7 @@ export function Header() {
     <div className="border-b">
       <div className="flex h-16 items-center gap-6 px-6">
         <Pizza className="h-6 w-6" />
+
         <Separator orientation="vertical" className="h-6" />
 
         <nav className="flex items-center space-x-4 lg:space-x-6">
@@ -20,6 +23,11 @@ export function Header() {
             Pedidos
           </NavLink>
         </nav>
+
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+          <AccountMenu />
+        </div>
       </div>
     </div>
   )
