a293e569657e1a4155d36aab5ab71db53c2c32a6
Bertoldo Klinger <bertoldokbrj@gmail.com>
2024-02-06 16:44:33 -0300

    feat: add error page on routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,9 +2,9 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
+import { NotFound } from './pages/404'
 import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
-import { NotFound } from './pages/auth/404'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
 
@@ -25,5 +25,9 @@ export const router = createBrowserRouter([
       { path: '/sign-in', element: <SignIn /> },
       { path: '/sign-up', element: <SignUp /> },
     ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ])
